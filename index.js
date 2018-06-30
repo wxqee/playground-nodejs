68d64553f3abf4a97dbfc623112e2bc97688f7b4
Wang Xiaoqiang <xiaoqiang.wang@storehub.com>
2018-07-01 02:26:41 +0800

    my-time-tracker: a time tracking module

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
-console.log('Hello, world!');
+const track = () => {
+	let _start = new Date();
+	return {
+		terminate() { return ((new Date() - _start) / 1000).toFixed(2); },
+		restart() { _start = new Date(); },
+	};
+};
+
+
+const tr = track();
+const tr2 = track();
+
+setTimeout(() => {
+	console.log(tr.terminate());
+}, 1500);
+
+setTimeout(() => {
+	console.log(tr2.terminate());
+}, 2500);
+
