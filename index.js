19354c3e37317622d75c46daf2d8565f115e7fb2
Wang Xiaoqiang <xiaoqiang.wang@storehub.com>
2018-07-01 01:39:01 +0800

    fs-extra: adds file system methods that aren't included in the native fs module and adds promise support to the fs methods

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
-console.log('Hello, world!');
+const fs = require('fs-extra')
+ 
+// Async with promises:
+fs.copy('./myfile', './mynewfile')
+  .then(() => console.log('success!'))
+  .catch(err => console.error(err))
+
