321e1e9364be44c3c5cd3d57e78bd744c9fb745e
Wang Xiaoqiang <xiaoqiang.wang@storehub.com>
2018-07-01 01:01:15 +0800

    try ora

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
-console.log('Hello, world!');
+const ora = require('ora');
+ 
+const spinner = ora('Loading unicorns').start();
+ 
+setTimeout(() => {
+    spinner.color = 'yellow';
+    spinner.text = 'Loading rainbows';
+}, 1000);
+
