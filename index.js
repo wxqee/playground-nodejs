303e1c35e106c17bb466a978efd35c0f665a6a6a
Wang Xiaoqiang <xiaoqiang.wang@storehub.com>
2018-07-01 01:08:40 +0800

    glob: Match files using the patterns the shell uses, like stars and stuff.

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
-console.log('Hello, world!');
+var path = require("path");
+var glob = require("glob")
+
+var options = {
+	cwd: path.join(process.cwd(), 'src'),
+};
+
+// options is optional
+glob("**/*.js", options, function (er, files) {
+  // files is an array of filenames.
+  // If the `nonull` option is set, and nothing
+  // was found, then files is ["**/*.js"]
+  // er is an error object or null.
+
+	if (er) {
+		console.log('no file found');
+		return false;
+	}
+
+	console.log('files', files);
+})
+
