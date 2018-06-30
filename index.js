11995d71da27f06e000f90f9c0b439669c78f1e4
Wang Xiaoqiang <xiaoqiang.wang@storehub.com>
2018-07-01 01:03:11 +0800

    log-symbols: Colored symbols for various log levels

--- index.js
+++ index.js
@@ -1,1 +1,6 @@
-console.log('Hello, world!');
+const logSymbols = require('log-symbols');
+ 
+console.log(logSymbols.success, 'Finished successfully!');
+// On good OSes:  ✔ Finished successfully!
+// On Windows:    √ Finished successfully!
+
